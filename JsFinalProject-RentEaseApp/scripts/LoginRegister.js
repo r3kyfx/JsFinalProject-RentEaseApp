2374ac59b7aedf28b9960bd018fe7bd3281bcc37
r3kyfx <106031382+r3kyfx@users.noreply.github.com>
2025-05-12 18:36:51 +0300

    Add files via upload

---
+++ JsFinalProject-RentEaseApp/scripts/LoginRegister.js
@@ -0,0 +1,82 @@
+import { checkPassword } from "./utils.js";
+import { User } from "./data.js";
+
+const regBtn = document.getElementById("regBtn");
+const regForm = document.getElementById("regForm")
+const logBtn = document.getElementById("logBtn")
+const container = document.getElementsByClassName("container")[0];
+const actualLogBtn = document.getElementById("actualLogBtn");
+const actualRegBtn = document.getElementById("actualRegBtn");
+
+//login
+const logEmail = document.getElementById("logMail");
+const logPass = document.getElementById("logPass");
+const logErrors = document.getElementsByClassName("logErrors")[0];
+
+//register
+const regEmail = document.getElementById("regEmail");
+const regPass = document.getElementById("regPass");
+const firstName = document.getElementById("firstName");
+const lastName = document.getElementById("lastName");
+const birthDate = document.getElementById("birthDate");
+const regErrors = document.getElementsByClassName("regErrors")[0];
+
+regBtn.addEventListener("click" , () => {
+    regForm.style.transform = "translateY(0%)";
+    container.style.backgroundColor = "#78C0E0";
+})
+
+logBtn.addEventListener("click" , () => {
+    regForm.style.transform = "translateY(-100%)";
+    container.style.backgroundColor = "#efecec";
+})
+
+actualLogBtn.addEventListener("click" , () => {
+    
+    const users = JSON.parse(localStorage.getItem("users")) || [];
+
+    const findUser = users.find((user) => (user.email === logEmail.value && user.password === logPass.value));
+    if(findUser){
+        window.location.href = "./HomePage.html";
+    }else{
+        logErrors.innerText = "Email or password invalid";
+    }
+})
+
+actualRegBtn.addEventListener("click" , () => {
+    const regInputs = Array.from(regForm.querySelectorAll("input"));
+    const clearInputs = regInputs.filter(input => input.value === "");
+    const filledInputs = regInputs.filter(input => input.value !== "");
+    filledInputs.forEach(input => input.style.borderColor = "black");
+    if(clearInputs.length){
+        regErrors.innerText = "All fields are required";
+        clearInputs.forEach(input => {
+            input.style.borderColor = "red";
+        })
+        return;
+    }
+    const passCheck = checkPassword(regPass.value);
+
+    if(passCheck !== ""){
+        regErrors.innerText = passCheck;
+        return
+    }
+    
+    const users = JSON.parse(localStorage.getItem("users")) || [];
+    const uploadUser = users.some((user) => user.email === regEmail.value);
+    if(!uploadUser){
+            let newUser = new User(
+            regEmail.value,
+            regPass.value,
+            firstName.value,
+            lastName.value,
+            birthDate.value
+        );
+
+        users.push(newUser);
+        localStorage.setItem("users" , JSON.stringify(users));
+    }else{
+        regErrors.innerText = "User already exists";
+        return;
+    }
+})
---
+++ JsFinalProject-RentEaseApp/scripts/utils.js
@@ -0,0 +1,12 @@
+function checkPassword(password){
+    let result = "Passowrd must contain at least one "
+    let upperCase = /[A-Z]/.test(password);
+    let nums = /[0-9]/.test(password);
+    let specialCh = /[^A-Za-z0-9]/.test(password);
+    if(!upperCase) return result += "uppercase letter";
+    if(!nums) return result += "number"
+    if(!specialCh) return result += "special character";
+    return ""
+}
+
+export {checkPassword}
